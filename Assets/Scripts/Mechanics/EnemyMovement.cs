d5984528b6361e0114bbf2b2cd52790b2cef60e8
stephdraggo <stephdraggo@users.noreply.github.com>
2020-09-14 12:32:48 +1000

    some movement

---
+++ Assets/Scripts/Mechanics/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mechanics
+{
+    [AddComponentMenu("Mechanics/Movement/Enemy")]
+    public class EnemyMovement : MonoBehaviour
+    {
+        #region Variables
+        private EnemyState state;
+        #endregion
+
+        #region Start
+        private void Start()
+        {
+            state = EnemyState.Passive;
+        }
+        #endregion
+
+        #region Update
+        private void Update()
+        {
+
+        }
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mechanics
+{
+    [AddComponentMenu("Mechanics/Movement/Player")]
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Variables
+        private PlayerState state;
+        #endregion
+
+        #region Start
+        private void Start()
+        {
+
+        }
+        #endregion
+
+        #region Update
+        private void Update()
+        {
+
+        }
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
