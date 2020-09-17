64f1647ce013b0b01055c3db70755a32df983ba6
Lala-banners <lala.l.banno@hotmail.com>
2020-09-17 15:00:43 +1000

    Created pause menu, script and game manager
    Moved into game scene

---
+++ Assets/Scripts/Menus/PauseHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour
+{
+    //Variables for pause menu
+    static bool isPaused;
+    public GameObject pauseMenu, optionsMenu;
+
+    public void Paused()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void UnPaused()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+        pauseMenu.SetActive (false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionsMenu.activeSelf)
+            {
+                optionsMenu.SetActive(true);
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                isPaused = !isPaused;
+                if (isPaused)
+                {
+                    Paused();
+                }
+                else
+                {
+                    UnPaused();
+                }
+            }
+        }
+    }
+
+  
+}
