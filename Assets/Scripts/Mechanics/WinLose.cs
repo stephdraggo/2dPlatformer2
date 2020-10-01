ef8f407f995e5c86018ade24996ab7dbf395543a
Lala-banners <lala.l.banno@hotmail.com>
2020-10-01 15:14:32 +1000

    Win/Lose almost done

---
+++ Assets/Scripts/Mechanics/WinLose.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLose : MonoBehaviour
+{
+    /*Score
+            * game score
+                -stars collected
+                -enemies killed/spared?
+
+            * temp(level) score
+                -discard if level failed
+                -add to game score if level succeeded
+    */
+
+    public int starCount = 0; //current stars collected
+    public int finishStarCount = 10; //stars needed to finish the level
+    public int sparedEnemy;
+    public int killedEnemy;
+    public GameObject winPanel;
+    public GameObject secretWin;
+
+    public void CompleteLevel()
+    {
+        //If stars collected is greater or equal to finished star count, then the congrats menu pops up
+        if (starCount >= finishStarCount)
+        {
+            winPanel.SetActive(true);
+        }
+    }
+
+    //Secret function for if player spares more enemies than killed
+    public void SecretWin()
+    {
+        if (sparedEnemy >= killedEnemy)
+        {
+            secretWin.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
